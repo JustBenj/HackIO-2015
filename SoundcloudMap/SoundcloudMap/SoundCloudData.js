463a14663a72e1b1141efd0fa261f3bb387a4890
Skywolf829 <swwurster@gmail.com>
2015-11-14 17:27:09 -0500

    Moving readme

--- SoundcloudMap/SoundcloudMap/SoundCloudData.js
+++ SoundcloudMap/SoundcloudMap/SoundCloudData.js
@@ -2,4 +2,8 @@ SC.initialize({
 	client_id: '92c4a7cc969806a86bb2004c626dfc99'
 });
 
-var city = SC.get('/users/
+var usersInTowns = [""];
+
+SC.get('/users', {
+		city: ''
+		}).then(usersInTowns.push(user));
