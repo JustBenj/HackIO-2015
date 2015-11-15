292eede54fcd41621bef54ac817fbb3ac6b347c4
Skywolf829 <swwurster@gmail.com>
2015-11-14 19:00:12 -0500

    Fetches users in Columbus, OH

--- SoundcloudMap/SoundcloudMap/SoundCloudData.js
+++ SoundcloudMap/SoundcloudMap/SoundCloudData.js
@@ -1,4 +1,15 @@
-
-var usersInTowns = ["lol", "test"];
+SC.initialize({
+		client_id: '92c4a7cc969806a86bb2004c626dfc99'
+	});
 	
-document.getElementById("test").innerHTML = "hi";
+	var page_size = 200;
+	var usersInTowns = ["hello", "hey"];
+		
+	SC.get('/users', { 
+		q: 'columbus, OH',
+		limit: page_size,
+	  	linked_partitioning: 1
+		}
+		).then(function(users) {
+			console.log(users);
+		});
